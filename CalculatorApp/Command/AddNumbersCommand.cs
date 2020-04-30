f328b36ced13f51ac67e9887a8ab19778fcbabdd
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-01 01:14:07 +0200

    implemented calculator app

---
+++ CalculatorApp/Command/AddNumbersCommand.cs
@@ -0,0 +1,41 @@
+﻿using CalculatorApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace CalculatorApp.Command
+{
+    class AddNumbersCommand : ICommand
+    {
+        private CalculatorViewModel _viewModel;
+
+        
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter == null)
+                return false;
+            else
+            {
+                _viewModel = (CalculatorViewModel)parameter;
+            }
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public void Execute(object parameter)
+        {
+            if(parameter == null)
+            {
+                if (_viewModel == null)
+                    throw new ArgumentNullException("parameter");
+            }
+            else
+            {
+                _viewModel = (CalculatorViewModel)parameter;
+            }
+            _viewModel.Sum = _viewModel.FirstNumber + _viewModel.SecondNumber;
+        }
+    }
+}
---
+++ CalculatorApp/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,68 @@
+﻿using CalculatorApp.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace CalculatorApp.ViewModel
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private int _firstNumber;
+        private int _secondNumber;
+        private int _sum;
+        private ICommand _AddNumbersCommand;
+
+        public ICommand AddNumbersCommand
+        {
+            get { return _AddNumbersCommand; }
+            set
+            {
+                _AddNumbersCommand = value;
+                OnPropertyChanged("AddNumbersCommand");
+            }
+        }
+        public int FirstNumber
+        {
+            get { return _firstNumber; }
+            set
+            {
+                _firstNumber = value;
+                OnPropertyChanged("FirstNumber");
+            }
+        }
+        public int SecondNumber
+        {
+            get { return _secondNumber; }
+            set
+            {
+                _secondNumber = value;
+                OnPropertyChanged("SecondNumber");
+            }
+        }
+
+        public int Sum
+        {
+            get { return _sum; }
+            set
+            {
+                _sum = value;
+                OnPropertyChanged("Sum");
+            }
+        }
+
+        public CalculatorViewModel()
+        {
+            _AddNumbersCommand = new AddNumbersCommand();
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
