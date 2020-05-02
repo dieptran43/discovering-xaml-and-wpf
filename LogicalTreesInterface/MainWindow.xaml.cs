b99424d0719d216b0b22a9480adcf4ed4272a664
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-02 20:57:29 +0200

    working on styling component at logical tree

---
+++ LogicalTreesInterface/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogicalTreesInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+            if (b != null)
+                b.Foreground = Brushes.Blue;
+        }
+
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+            if (b != null)
+                b.Foreground = Brushes.Black;
+        }
+    }
+}
