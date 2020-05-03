c443d55d4e625f0400864fdaef1ff2854e3dc3c1
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-04 00:27:12 +0200

    working with application lifecycle flow

--- SpinThePrizeWheel/MainWindow.xaml.cs
+++ SpinThePrizeWheel/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,8 +26,28 @@ namespace SpinThePrizeWheel
             InitializeComponent();
             this.InputBindings.Add(
                 new KeyBinding(ApplicationCommands.Help, new KeyGesture(Key.F2)));
-     
         }
+
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            if (MessageBox.Show("Are you sure you want to close this app?",
+                "Annoying Prompt",
+                MessageBoxButton.YesNo, 
+                MessageBoxImage.Question)
+                == MessageBoxResult.No)
+                e.Cancel = true;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            MessageBox.Show("Hello!");
+        }
+
         void HelpCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
