794f371066d6e4fb9bf072744ebbf16eb3533ef3
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-03 22:59:44 +0200

    handling events on form in code behind

--- LogicalTreesInterface/MainWindow.xaml.cs
+++ LogicalTreesInterface/MainWindow.xaml.cs
@@ -23,6 +23,11 @@ namespace LogicalTreesInterface
         public MainWindow()
         {
             InitializeComponent();
+            this.AddHandler(Window.MouseRightButtonDownEvent,
+                new MouseButtonEventHandler(MainWindow_MouseRightButtonDown), true);
+            this.AddHandler(ListBox.SelectionChangedEvent,
+                new SelectionChangedEventHandler(ListBox_SelectionChanged));
+            this.AddHandler(Button.ClickEvent, new RoutedEventHandler(Button_Click));
         }
 
         private void Button_MouseEnter(object sender, MouseEventArgs e)
@@ -32,12 +37,41 @@ namespace LogicalTreesInterface
                 b.Foreground = Brushes.Blue;
         }
 
+        void MainWindow_MouseRightButtonDown(object sender,MouseButtonEventArgs e)
+        {
+            this.Title = "Source = " + e.Source.GetType().Name + ", OriginalSource = " +
+                e.OriginalSource.GetType().Name + " @ " + e.Timestamp;
+
+            Control source = e.Source as Control;
+
+            if(source.BorderThickness != new Thickness(5))
+            {
+                source.BorderThickness = new Thickness(5);
+                source.BorderBrush = Brushes.Black;
+            }
+            else
+            {
+                source.BorderThickness = new Thickness(0);
+            }
+        }
+
 
         private void Button_MouseLeave(object sender, MouseEventArgs e)
         {
             Button b = sender as Button;
             if (b != null)
                 b.Foreground = Brushes.Black;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count > 0)
+                MessageBox.Show("You just selected "+e.AddedItems[0]);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You just clicked " +e.Source);
         }
     }
 }
