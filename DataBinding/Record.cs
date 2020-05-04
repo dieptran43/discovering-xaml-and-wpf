8f01787db2c279f34c3b8a8b8a040b3a51226f8c
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-04 23:47:06 +0200

    added data binding simple project

---
+++ DataBinding/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBinding
+{
+    public class Record
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Uri WebSite { get; set; }
+        public bool IsBillionaire { get; set; }
+        public Gender Gender { get; set; }
+    }
+    
+}
